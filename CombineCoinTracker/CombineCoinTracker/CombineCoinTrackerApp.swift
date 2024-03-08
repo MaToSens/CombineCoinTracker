eb7c108d9873171c66fb65dc3ed28abacb170fe2
MaToSens <matoosens@gmail.com>
2024-03-08 23:16:47 +0100

    Added initial files

---
+++ CombineCoinTracker/CombineCoinTracker/CombineCoinTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineCoinTrackerApp.swift
+//  CombineCoinTracker
+//
+//  Created by MaTooSens on 08/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct CombineCoinTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
