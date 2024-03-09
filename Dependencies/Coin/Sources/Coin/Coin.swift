54a279eab0a06d10358e84e9c72f1502f87f0205
MaToSens <matoosens@gmail.com>
2024-03-09 11:11:47 +0100

    Added CoinManager conforming to CoinManagerInterface, utilizing CoinsDataProvider to Coin package

--- Dependencies/Coin/Sources/Coin/Coin.swift
+++ Dependencies/Coin/Sources/Coin/Coin.swift
@@ -5,4 +5,22 @@
 //  Created by MaTooSens on 08/03/2024.
 //
 
+import Backend
+import CoinInterface
+import DependencyInjection
 import Foundation
+
+public struct Dependencies {
+    public static func inject() {
+//        Assemblies.inject(type: CoinManagerInterface.self, object: CoinManager())
+        Assemblies.inject(type: CoinsDataProviderInterface.self, object: CoinsDataProvider())
+        
+        injectShared()
+    }
+    
+    public static func injectShared() {
+        Assemblies.inject(type: CoinManagerInterface.self, object: CoinManager())
+        
+        Backend.Dependencies.inject()
+    }
+}
