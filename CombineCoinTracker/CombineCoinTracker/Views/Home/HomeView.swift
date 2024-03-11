9b6f36f5005764fa4ae335c6eb9760d5eb22ad9c
MaToSens <matoosens@gmail.com>
2024-03-11 12:53:55 +0100

    Updated HomeView/ViewModel

--- CombineCoinTracker/CombineCoinTracker/Views/Home/HomeView.swift
+++ CombineCoinTracker/CombineCoinTracker/Views/Home/HomeView.swift
@@ -5,7 +5,9 @@
 //  Created by MaTooSens on 09/03/2024.
 //
 
+import Design
 import CoinInterface
+import Components
 import SwiftUI
 
 struct HomeView: View {
@@ -21,14 +23,57 @@ struct HomeView: View {
             Label("Error: \(error.localizedDescription)", systemImage: "questionmark")
         }
     }
-    
+        
     private func buildHomeView(_ coins: [CoinModel]) -> some View {
-        ScrollView {
-            VStack {
-                ForEach(coins) { coin in
-                    Text(coin.name)
-                }
+        ZStack {
+            Color.backgroundColor.ignoresSafeArea()
+            
+            VStack(spacing: 15) {
+                buildPortfolioBalanceSection()
+                buildPorfolioSnapshotSection(coins)
+                buildWatchListSection(coins)
             }
+            .padding()
+        }
+    }
+    
+    // MARK: Sections
+    private func buildPortfolioBalanceSection() -> some View {
+        VStack {
+            SectionHeaderView(title: "Portflio balance", buttonTitle: "USD") {
+                // Action
+            }
+            
+            Text("$0.00")
+                .font(.headline.weight(.semibold))
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Divider()
+        }
+    }
+    
+    private func buildPorfolioSnapshotSection(_ coins: [CoinModel]) -> some View {
+        VStack(spacing: 15) {
+            SectionHeaderView(title: "Portflio", buttonTitle: "View all") {
+                // Action
+            }
+            
+            HStack {
+                CoinSnapshotView(coins[0])
+                CoinSnapshotView(coins[1])
+            }
+            
+            Divider()
+        }
+    }
+    
+    private func buildWatchListSection(_ coins: [CoinModel]) -> some View {
+        VStack(spacing: 15) {
+            SectionHeaderView(title: "Watch list", systemName: "gear") {
+                // Action
+            }
+                
+            CoinListView(coins)
         }
     }
 }
--- Dependencies/Design/Sources/Design/Extensions/Color+Extension.swift
+++ Dependencies/Design/Sources/Design/Extensions/Color+Extension.swift
@@ -1,8 +1,27 @@
 //
-//  File.swift
-//  
+//  Color+Extension.swift
+//
 //
 //  Created by MaTooSens on 11/03/2024.
 //
 
-import Foundation
+import SwiftUI
+
+public extension Color {
+    init(
+        red: Double,
+        green: Double,
+        blue: Double,
+        opacity: Double = 1
+    ) {
+        self.init(
+            .sRGB,
+            red: red/255,
+            green: green/255,
+            blue: blue/255,
+            opacity: opacity
+        )
+    }
+    
+    static let backgroundColor = Color(red: 249, green: 259, blue: 251)
+}
